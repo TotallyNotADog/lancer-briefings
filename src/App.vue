a51a3b44feb863e4f5c9437b11e766b0f7c1d50a
TotallyNotADog <TotallyNotYourDog@gmail.com>
2023-10-04 16:33:42 -0500

    everything

--- src/App.vue
+++ src/App.vue
@@ -89,56 +89,76 @@ export default {
 
   data() {
     return {
-      "mission_slug": "001",
+      "mission_slug": "002",
       "current_md": "",
       "events": "",
       "missions": [
         {
           "slug": "001",
           "name": "Bug-Hunt",
+          "status": "success"
+        },
+        {
+          "slug": "002",
+          "name": "Vigilant Gaze",
+          "status": "partial-success"
+        },
+        {
+          "slug": "003",
+          "name": "Floodgate",
+          "status": "success"
+        },
+        {
+          "slug": "004",
+          "name": "Rallying Cry",
+          "status": "partial-success"
+        },
+        {
+          "slug": "005",
+          "name": "Split-Knuckle Haymaker",
           "status": "start"
         },
       ],
       "pilots": [
         {
-          "callsign": "Jinzo",
-          "alias": "Shayan Arsenal",
+          "callsign": "Vulcan",
+          "alias": "Hypatia Draconis",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "corpro": "HA",
+          "frame": "Genghis MK 1",
+          "mech": "The Black Dahlia"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "Templar",
+          "alias": "UNKNOWN",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Necrophile"
+          "corpro": "IPS-N",
+          "frame": "Lancaster",
+          "mech": "Bailey"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
+          "callsign": "Big Big Buh",
+          "alias": "Claudio Kilgannon",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "corpro": "IPS-N",
+          "frame": "Nelson",
+          "mech": "Super Duper Cool Man"
         },
         {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
-          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Mayfly"
-        },
-        {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
+          "callsign": "Heavenly Father",
+          "alias": 'Oddmund Lavan',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Rio Bravo"
+          "corpro": "IPS-N",
+          "frame": "Caliban",
+          "mech": "Primordial Spirit"
+        },
+        {
+          "callsign": "Soul",
+          "alias": "Kali",
+          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
+          "corpro": "HORUS",
+          "frame": "Goblin",
+          "mech": "Ghoul"
         },
       ],
       "header": {
@@ -147,10 +167,10 @@ export default {
         "system": "Ardennes-3",
         "gate": "Atlas-Quanokrim",
         "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
-        "headerSubtitle": "Mercenary Company",
-        "subheaderTitle": "Crisis Response",
-        "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
+        "headerTitle": "Landmark",
+        "headerSubtitle": "Colonial",
+        "subheaderTitle": "Crisis Response Mode",
+        "subheaderSubtitle": "Active",
       },
       "options":{
         "eventsMarkdownPerMission": true
